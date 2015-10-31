37be96e83f87a8c7b9a3f8f71432ecc8bda590db
amitsandesara <amitsandesara92@gmail.com>
2015-10-31 17:46:42 -0400

    App menu created

--- app/src/main/java/com/nyu/cs9033/travelbuddy/Controllers/HomeActivity.java
+++ app/src/main/java/com/nyu/cs9033/travelbuddy/Controllers/HomeActivity.java
@@ -3,10 +3,13 @@ package com.nyu.cs9033.travelbuddy.Controllers;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuInflater;
 
 import com.nyu.cs9033.travelbuddy.R;
 
 public class HomeActivity extends AppCompatActivity {
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -15,7 +18,13 @@ public class HomeActivity extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-
     }
 
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.menu_home, menu);
+        return true;
+        
+    }
 }
